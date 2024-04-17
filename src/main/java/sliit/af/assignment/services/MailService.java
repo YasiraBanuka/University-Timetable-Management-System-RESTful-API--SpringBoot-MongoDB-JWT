edbbd2e2105d1bebb98af357cf12d90bd320ef3c
Yasira Banuka <it21378270@my.sliit.lk>
2024-04-17 07:48:59 +0530

    mail service configured & added

---
+++ src/main/java/sliit/af/assignment/services/MailService.java
@@ -0,0 +1,23 @@
+package sliit.af.assignment.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class MailService {
+
+    private final JavaMailSender javaMailSender;
+
+    public void sendMail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("registration@colombouni.lk");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+
+}
